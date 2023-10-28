be461222a19e45fc7213eca0002828df659ee398
Renato Xavier Pedroso <renatoxpedroso@gmail.com>
2023-10-28 06:51:09 -0300

    Commit primeiro projeto CODER

---
+++ projeto_perguntas/lib/main.dart
@@ -0,0 +1,48 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  const PerguntaApp({super.key});
+  
+  void responder() {
+    // ignore: avoid_print
+    print('Pergunta respondida!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
