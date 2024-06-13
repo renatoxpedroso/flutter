b34711a2bafe53cda75280ef5797ef4062687ed5
Renato Xavier Pedroso <renatoxpedroso@gmail.com>
2024-06-13 20:03:52 -0300

    Orientação a Objetos em Dart

--- DIO/meu_app/bin/meu_app.dart
+++ DIO/meu_app/bin/meu_app.dart
@@ -6,6 +6,7 @@ import 'package:meu_app/functions/functions4_fatorial.dart' as app_functions_4_f
 import 'package:meu_app/functions/functions5_optional.dart' as functions_5_optional;
 import 'package:meu_app/functions/functions6_arrow.dart' as functions_6_arrow;
 import 'package:meu_app/functions/functions7_function_as_parameter.dart' as functions_7_function_as_parameter;
+import 'package:meu_app/models/carro.dart' as models_carro;
 
 void main(List<String> arguments) {
   //app_for.main(arguments);
@@ -18,5 +19,6 @@ void main(List<String> arguments) {
   //app_functions_4_fatorial.main(arguments);
   //functions_5_optional.main(arguments);
   //functions_6_arrow.main(arguments);
-  functions_7_function_as_parameter.main(arguments);
+  //functions_7_function_as_parameter.main(arguments);
+  //models_carro.main(arguments);
 }
---
+++ DIO/meu_app_oo/lib/service/impl/NotificacaoEmail.dart
@@ -0,0 +1,11 @@
+
+import 'package:meu_app_oo/service/NotificacaoInterface.dart';
+
+import '../../classes/pessoa_abstract.dart';
+
+class NotificacaoEmail implements NotificacaoInterface {
+  @override
+  void enviarNotificacao(Pessoa pessoa) {
+    print("Enviando Email para: ${pessoa.getNome()}");
+  }
+}
